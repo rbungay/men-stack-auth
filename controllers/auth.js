1d9bae3363696f3e38be316204fef5c9745e4ed5
Rani Bungay <ranibungay@gmail.com>
2024-09-30 11:40:36 -0400

    completed authentication

--- controllers/auth.js
+++ controllers/auth.js
@@ -7,7 +7,7 @@ import bcrypt from "bcrypt";
 
 //to get to the sign up place
 router.get("/sign-up", (req, res) => {
-  res.render("auth/sign-up.ejs");
+  res.render("auth/sign-up.ejs", { user: req.session.user });
 });
 
 //to POST TO THE ACTUAL PAGE
@@ -25,6 +25,7 @@ router.post("/sign-up", async (req, res) => {
   req.body.password = hashedPassword;
 
   const user = await User.create(req.body);
+  req.session.user = user;
   //   res.send(`Thanks for signing up ${user.username}`);
 
   res.render("index.ejs");
@@ -36,11 +37,13 @@ router.get("/sign-in", (req, res) => {
 });
 
 router.post("/sign-in", async (req, res) => {
+  // First, get the user from the database
   const userInDatabase = await User.findOne({ username: req.body.username });
   if (!userInDatabase) {
     return res.send("Login failed. Please try again.");
   }
-  // returns a boolean to confirm if they are signing in matches.
+
+  // There is a user! Time to test their password with bcrypt
   const validPassword = bcrypt.compareSync(
     req.body.password,
     userInDatabase.password
@@ -49,7 +52,22 @@ router.post("/sign-in", async (req, res) => {
     return res.send("Login failed. Please try again.");
   }
 
-  res.send(`You logged in successfully! Welcome ${userInDatabase.username}`);
+  // There is a user AND they had the correct password. Time to make a session!
+  // Avoid storing the password, even in hashed format, in the session
+  // If there is other data you want to save to `req.session.user`, do so here!
+  req.session.user = {
+    username: userInDatabase.username,
+    _id: userInDatabase._id,
+  };
+
+  // res.send(`You logged in successfully! Welcome ${userInDatabase.username}`);
+  res.redirect("/");
+});
+
+//signs out user
+router.get("/sign-out", (req, res) => {
+  req.session.destroy();
+  res.redirect("/");
 });
 
 export default router;
--- server.js
+++ server.js
@@ -15,6 +15,7 @@ import mongoose from "mongoose";
 import methodOverride from "method-override";
 import morgan from "morgan";
 import authController from "./controllers/auth.js"; // NEED TO CONFIRM IF THIS IS HOW YOU WRITE IT
+import session from "express-session";
 
 // Set the port from environment variable or default to 3000
 const port = process.env.PORT ? process.env.PORT : "3000";
@@ -27,13 +28,32 @@ app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride("_method"));
 // Morgan for logging HTTP requests
 app.use(morgan("dev"));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
 //importing middleware
 app.use("/auth", authController);
 
+//expresssession middleware
+
 //Defining a route for index.ejs
-app.get("/", async (req, res) => {
-  res.render("index.ejs");
+app.get("/", (req, res) => {
+  res.render("index.ejs", {
+    user: req.session.user,
+  });
+});
+
+app.get("/vip-lounge", (req, res) => {
+  if (req.session.user) {
+    res.send(`Welcome to the party ${req.session.user.username}.`);
+  } else {
+    res.send("Sorry, no guests allowed.");
+  }
 });
 
 mongoose.connection.on("connected", () => {
